008032826bf4e99f3b1e61786f85e6852dc03bdb
Guy_Weintraub <guywine@gmail.com>
2020-09-30 23:09:33 +0300

    create files

---
+++ gene_sets.py
@@ -0,0 +1,5 @@
+'''
+this will provide the user to choose which gene set to get.
+- groups from table: by ATAC_bigtable (hrde-1 score, H3K9, mRNA level...)
+- a set of the most / least 10% of ATAC_contin_param for a desired condition (choose percentage)
+'''
---
+++ plot_atac_signal.py
@@ -0,0 +1,7 @@
+'''
+Plot ATAC-seq signal. Choose:
+- Experiment: exp1 / exp_gonads / exp_hrde_gonads / exp_hrde_guy / exp_metsetset
+- set of genes: list
+- Normalize: highly_lowly / none
+- Mean all replicates or display them seperately?
+'''
