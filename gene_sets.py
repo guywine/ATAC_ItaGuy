d3e1fb81baff5699bd860c6305ad341d0ad68aef
Guy_Weintraub <guywine@gmail.com>
2020-10-05 14:34:21 +0300

    remove zeros from wanted columns

--- gene_sets.py
+++ gene_sets.py
@@ -3,12 +3,24 @@ This will provide the user to choose which gene set to get.
 - groups from table: by ATAC_bigtable (hrde-1 score, H3K9, mRNA level...)
 - a set of the most / least 10% of ATAC_FC for a desired condition (choose percentage)
 '''
-import pandas as pd 
+import pandas as pd
+import numpy as np
 
 def read_big_table():
+    '''
+    Reads big_table of ATAC project, removes first header row, sets wbid to index.
+    Replaces 0 with NaN in the desired columns.
+
+    Return
+    ---------
+    - big_table: pd.DataFrame. index is wbid.
+    '''
     big_table = pd.read_excel('ATAC_bigtable_1.xlsx', header=1)
     big_table.set_index('gene', inplace=True)
+
     # clean zeros from Ketting and Kennedy
+    cols_to_drop_zero = ['isHrde1', 'ketting_score_all', 'ketting_score_20_23']
+    big_table[cols_to_drop_zero] = big_table[cols_to_drop_zero].replace(0,np.nan)
     return big_table
 
 
@@ -42,12 +54,12 @@ def get_list(big_table, col_name:str, prcnt:float=0, upper:bool = True):
 
 
 if __name__=='__main__':
-    print('hey')
     big_table = read_big_table()
-    # clean zeros from Ketting and Kennedy
+
+    ## add option of all genes
     col_name = 'isHrde1'
     ids_list = get_list(big_table, col_name)
     ids_list_2 = get_list(big_table, col_name, prcnt=10)
     ids_list_3 = get_list(big_table, col_name, prcnt=20, upper=False)
 
-    print('hey')
+    print('done')
