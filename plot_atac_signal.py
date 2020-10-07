1f6b252ed27dd36d8e8785fa49eed77b0acd1234
Guy_Weintraub <guywine@gmail.com>
2020-10-07 18:55:38 +0300

    finished the "long" form, plot_new function

--- plot_atac_signal.py
+++ plot_atac_signal.py
@@ -59,33 +59,6 @@ def plot_new(exp_dic: dict, dic_groups: dict, conditions: tuple = ("c1", "c2"), 
             # plot
             plot_panel(df_means_list_a[rep_i], df_means_list_b[rep_i], conditions, compare)
 
-
-# def plot_replicates(
-#     reps_dic: dict, dic_groups: dict, conditions: tuple = ("condition 1", "condition 2")
-# ):
-#     """
-#     Gets a dict with all samples (dfs) ordered by replicates,
-#     and the desired groups of genes, and plots them seperately.
-#     """
-#     for rep_num in reps_dic:
-#         print(f"Replicate {rep_num} :")
-    
-#         ## get mean of groups for first condition
-#         cond_a_df = reps_dic[rep_num][0]
-#         df_means_a = mean_gene_groups_of_sample(
-#             sample_df=cond_a_df, dic_groups=dic_groups
-#         )
-
-#         ## get mean of groups for second condition
-#         cond_b_df = reps_dic[rep_num][1]
-#         df_means_b = mean_gene_groups_of_sample(
-#             sample_df=cond_b_df, dic_groups=dic_groups
-#         )
-
-#         ## plot
-#         plot_panel(df_means_a, df_means_b, conditions)
-
-
 def plot_panel(df_means_a, df_means_b, conditions: tuple=('cond1', 'cond2'), compare:bool=False):
     '''
     Can plot panel with:
@@ -93,12 +66,11 @@ def plot_panel(df_means_a, df_means_b, conditions: tuple=('cond1', 'cond2'), com
     - 1 ax, with both conditions, when the condition name is added to the group name
     '''
     if compare:
-        fig, axes = plt.subplots(nrows=1, ncols=1, sharey=True, figsize=(4, 4))
-
-        add_cond_to_col_names(df=df_means_a, condition=conditions[0])
-        add_cond_to_col_names(df=df_means_b, condition=conditions[1])
-        df_both_conds = pd.concat([df_means_a, df_means_b])
-        df_both_conds.plot(ax=axes)
+        # combine dfs to long ['group','signal','condition']
+        long_ab = combine_conditions_to_long(df_means_a, df_means_b, conditions)
+        
+        fig, axes = plt.subplots()
+        sns.lineplot(data=long_ab, x=long_ab.index, y='signal', hue='group', style='condition', ax=axes)
 
     else:
         fig, axes = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(9, 4))
@@ -144,24 +116,19 @@ def get_df_means_list(reps_dic: dict, dic_groups: dict, cond_num:int):
         df_means_list.append(df_means)
     return df_means_list
 
-# def plot_all_replicates_mean(reps_dic: dict, dic_groups: dict, conditions: tuple = ("condition 1", "condition 2")):
-#     '''
-#     ### compute mean of samples and std (as shadow) between samples.
-#     ### https://seaborn.pydata.org/generated/seaborn.lineplot.html
-#     '''
-#     print('Mean of all repeats:')
-#     df_mean_all_reps_a, df_std_a = get_mean_of_groups_all_replicates(reps_dic, dic_groups, cond_num=0)
-#     df_mean_all_reps_b, df_std_b = get_mean_of_groups_all_replicates(reps_dic, dic_groups, cond_num=1)
-#     plot_panel(df_mean_all_reps_a, df_mean_all_reps_b, conditions)
 
+def combine_conditions_to_long(df_a, df_b, conditions:tuple=('a','b')):
+    '''
+    '''
+    a_long = df_a.melt(var_name='group',value_name='signal', ignore_index=False)
+    b_long = df_b.melt(var_name='group',value_name='signal', ignore_index=False)
 
-def add_cond_to_col_names(df:pd.DataFrame, condition:str='treated'):
-    '''
-    Changes inplace.
-    '''
-    new_cols = [f'{name} {condition}' for name in df.columns]
-    cols_dict = dict(zip(df.columns, new_cols))
-    df.rename(columns=cols_dict, inplace=True)
+    a_long['condition']=conditions[0]
+    b_long['condition']=conditions[1]
+
+    combined_ab_df = pd.concat([a_long, b_long])
+
+    return combined_ab_df
 
 
 
@@ -171,43 +138,39 @@ if __name__ == "__main__":
 
     ## generate lists of genes to be plotted
     gs = Gene_sets()
-
     dic_list = {
         "hrde-1": ["isHrde1"],
         "pol-2": ["isPol2"],
         "highly": ["R1-SX_S14", 10],
         "all genes": ["ALL"],
     }
+    # dic_groups = gs.get_multiple_lists(dic_list)
 
-    dic_groups = gs.get_multiple_lists(dic_list)
-
-    # oma_1_gene = ['WBGene00003864']
-    # dic_groups = {'oma-1 gene': oma_1_gene}
-
-    # plot_replicates(
-    #     exp1_dic, dic_groups, conditions=("anti-gfp RNAi", "anti-oma-1 RNAi")
-    # )
+    oma_1_gene = ['WBGene00003864']
+    dic_groups = {'oma-1 gene': oma_1_gene}
 
     plot_new(exp1_dic, dic_groups, conditions=("anti-gfp RNAi", "anti-oma-1 RNAi"), mean_all=False)
     plot_new(exp1_dic, dic_groups, conditions=("anti-gfp RNAi", "anti-oma-1 RNAi"), mean_all=True)
     plot_new(exp1_dic, dic_groups, conditions=("anti-gfp RNAi", "anti-oma-1 RNAi"), mean_all=False, compare=True)
+    plot_new(exp1_dic, dic_groups, conditions=("anti-gfp RNAi", "anti-oma-1 RNAi"), mean_all=True, compare=True)
 
 
 
-    # plot_all_replicates_mean(exp1_dic, dic_groups, conditions=('gfp RNAi','oma-1 RNAi'))
 
     ### work with four concat tables to create STD?
-    # df_means_list = get_df_means_list(exp1_dic, dic_groups, cond_num=0)
-    # means_concat = pd.concat(df_means_list)
-    # means_computed_a = means_concat.groupby(level=0).mean()
+    # df_means_list_a = get_df_means_list(exp1_dic, dic_groups, cond_num=0)
+    # means_concat_a = pd.concat(df_means_list_a)
+    # a_long = means_concat_a.melt(var_name='group',value_name='signal', ignore_index=False)
+    # a_long['condition']='a'
 
     # df_means_list_b = get_df_means_list(exp1_dic, dic_groups, cond_num=1)
     # means_concat_b = pd.concat(df_means_list_b)
-    # means_computed_b = means_concat_b.groupby(level=0).mean()
+    # b_long = means_concat_b.melt(var_name='group',value_name='signal', ignore_index=False)
+    # b_long['condition']='b'
 
-    # means_computed_a['cond']='a'
-    # means_computed_b['cond']='b'
+    # a_b_df = pd.concat([a_long, b_long])
 
+    # sns.lineplot(data=a_b_df, x=a_b_df.index, y='signal', hue='group', style='condition')
 
 
     
