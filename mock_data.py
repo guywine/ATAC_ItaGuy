c69fd8933b11803042c4ef7b47d38211be02b6a9
Guy_Weintraub <guywine@gmail.com>
2020-10-07 15:20:38 +0300

    created "mock" script

---
+++ mock_data.py
@@ -0,0 +1,13 @@
+
+
+def generate_mock_groups():
+    gene_list_1 = ["WBGene00007063", "WBGene00007064", "WBGene00007067"]
+    gene_list_2 = [
+        "WBGene00017071",
+        "WBGene00019895",
+        "WBGene00009583",
+        "WBGene00018682",
+    ]
+
+    dic_groups = {"group 1": gene_list_1, "group 2": gene_list_2}
+    return dic_groups
