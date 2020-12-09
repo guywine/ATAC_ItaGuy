c6a3ce4aa553022515d8c9652c3f877f3f6605d0
Guy_Weintraub <guywine@gmail.com>
2020-12-09 11:16:49 +0200

    ...

--- playing.py
+++ playing.py
@@ -31,7 +31,7 @@ if __name__=='__main__':
         hrde_up = gs.get_list('mRNA_log2_FC', thresh=0)
         hrde_up_sig = ut.intersect_lists(hrde_FC_sig, hrde_up)
         hrde_regulated = ut.intersect_lists(hrde_up_sig, hrde1_kennedy)
-        df_mRNA_and_hrde1 = gs.big_table.loc[hrde_regulated,['mRNA_log2_FC','hrde-1-Kennedy']]
+        df_mRNA_and_hrde1 = gs.big_table.loc[hrde_regulated, ['mRNA_log2_FC','hrde-1-Kennedy']]
 
     ## low rna:
     if True: 
@@ -70,7 +70,57 @@ if __name__=='__main__':
         low_closed_hrde1 = ut.intersect_lists(low_rna_low_atac, hrde_regulated)
 
     if True:
-        low_rna_low_atacscreen_chromosome
+        low_low_not_hrde1 = ut.intersect_lists(low_rna_low_atac, hrde1_kennedy, 'only first')
+        low_low_not_hrde1_middle = ut.screen_gene_location(low_low_not_hrde1)
+    
+    ### remove the genes that are too low in our atac
+    if True:
+        atac_ours_gfp_low20 = ut.get_list_of_column(
+            igfp_mean_scores, prcnt=20, bottom=True
+        )
+
+        atac_ours_oma1_low20 = ut.get_list_of_column(
+            ioma1_mean_scores, prcnt=20, bottom=True
+        )  #
+
+        atac_ours_low20 = ut.intersect_lists(atac_ours_gfp_low20, atac_ours_oma1_low20)
+
+        low_rna_low_atac_not_hrde1_cutoff_bottom = ut.intersect_lists(low_low_not_hrde1_middle, atac_ours_low20, 'only first')
+    
+    ##### high and open
+    if True:
+        # rna high
+        rna_ours_top10 = ut.get_list_of_column(m.mean_exp, prcnt=10) # 2029
+        rna_ahri_top10 = ut.get_list_of_column(ar.rna['Germline'], prcnt=10) # 2023
+
+        rna_top10 = ut.intersect_lists(rna_ours_top10, rna_ahri_top10) #1087
+
+        # germline 1.st
+        germline_1st_df = ar.rna[ar.rna['1st.max.tissue']=='Germline']
+        germline_1st = germline_1st_df.index.tolist() # 7677
+
+        # atac 
+        atac_ours_gfp_20 = ut.get_list_of_column(igfp_mean_scores, prcnt=20)  #
+        atac_ours_oma1_20 = ut.get_list_of_column(ioma1_mean_scores, prcnt=20)  #
+        atac_ours_20 = ut.intersect_lists(atac_ours_gfp_20, atac_ours_oma1_20)  #  
+
+        atac_ahri_20 = ut.get_list_of_column(ar.atac["Germline"], prcnt=20)  # 
+
+        atac_top20 = ut.intersect_lists(atac_ours_20, atac_ahri_20) # 1059
+
+        # both:
+        high_high = ut.intersect_lists(atac_top20, rna_top10) # 275
+        high_high_middle = ut.screen_gene_location(high_high) # 217
+        high_high_middle_names = ut.list_to_name(high_high_middle)
+
+
+
+
+
+
+
+
+
 
     
 
