b14f7016ee4d661141258a5fb6d37cf9a6b2a6af
Guy_Weintraub <guywine@gmail.com>
2021-01-29 13:03:21 +0200

    cleaned

--- read_tables.py
+++ read_tables.py
@@ -5,9 +5,84 @@ from typing import Union
 import pathlib
 import seaborn as sns
 
+def read_experiment_to_df(exp_name: str = "exp1"):
+    exp_dfs_cond1, exp_dfs_cond2 = read_experiment(exp_name)
+    exp_df = create_exp_df(exp_dfs_cond1, exp_dfs_cond2, exp_name)
+    return exp_df
+
+
+def read_experiment(exp_name: str = "exp1"):
+    """
+    Gets an experiment name, reads all of the relevant tables to two lists, one for each condition.
+
+    exp_names can be: 'exp1' / 'exp_gonads' / 'exp_hrde_gonads' / 'exp_hrde_guy' / 'exp_metsetset'
+
+    Return
+    -------
+    - exp_dfs_cond1: list of dfs, each is a sample of the first condition (gfp / hrde-1;SX)
+    - exp_dfs_cond1: list of dfs, each is a sample of the second condition (oma-1 / SX)
+    """
+    #### file_names
+    exp1_files = [
+        ["DATA/exp1/ATAC_R0-iGFP.csv",
+        "DATA/exp1/ATAC_R1-iGFP.csv",
+        "DATA/exp1/ATAC_R2-iGFP.csv",
+        "DATA/exp1/ATAC_R4-iGFP.csv"],
+        ["DATA/exp1/ATAC_R0-iOMA-1.csv",
+        "DATA/exp1/ATAC_R1-iOMA-1.csv",
+        "DATA/exp1/ATAC_R2-iOMA-1.csv",
+        "DATA/exp1/ATAC_R4-iOMA-1.csv"]
+    ]
+    exp_gonads_files = []  # to fill later
+    exp_hrde_gonads_files = []  # to fill later
+    exp_hrde_guy_files = []  # to fill later
+    exp_metsetset_files = []  # to fill later
+
+    exps_dict = {
+        "exp1": exp1_files,
+        "exp_gonads": exp_gonads_files,
+        "exp_hrde_gonads": exp_hrde_gonads_files,
+        "exp_hrde_guy": exp_hrde_guy_files,
+        "exp_metsetset": exp_metsetset_files,
+    }
+
+    exp_dfs_cond1 = []
+    exp_dfs_cond2 = []
+    num_of_reps = len(exps_dict[exp_name][0])
+    for rep_i in range(num_of_reps):
+        exp_dfs_cond1.append(read_and_format_atac_table(exps_dict[exp_name][0][rep_i]))
+        exp_dfs_cond2.append(read_and_format_atac_table(exps_dict[exp_name][1][rep_i]))
+
+    return exp_dfs_cond1, exp_dfs_cond2
+
+
+def create_exp_df(exp_dfs_cond1: list, exp_dfs_cond2: list, exp_name: str):
+    '''
+    Gets two lists of dfs, each list is samples of a condition.
+    Puts all df_samples in a single df of dfs:
+        - Row: rep_number
+        - Col: condition name 
+            * ['anti gfp' and 'anti OMA-1'] or [hrde-1;SX' and 'SX']
+    '''
+    columns_dic = {'cond1':exp_dfs_cond1, 'cond2':exp_dfs_cond2}
+    exp_df = pd.DataFrame(columns_dic)
+    
+    if exp_name in ['exp1', 'exp_gonads', 'exp_metsetset']:
+        conds = {'cond1':'anti gfp','cond2':'anti OMA-1'}
+    elif exp_name in ['exp_hrde_gonads', 'exp_hrde_guy']:
+        conds = {'cond1':'hrde-1;SX','cond2':'SX'}
+    else:
+        NameError, 'exp name not recognized'
+    
+    exp_df.rename(columns=conds, inplace=True)
+    exp_df.index.name = 'rep'
+
+    return exp_df
+
+
 def read_and_format_atac_table(f_name: Union[pathlib.Path, str]):
     """
-    Reads table from file (if exists)
+    Reads table of a single sample from file (if exists)
     Removes un needed columns
     Sets wbid as index
 
@@ -40,110 +115,54 @@ def read_atac_table(f_name: Union[pathlib.Path, str]):
     atac_table = pd.read_csv(f_path, header=None, index_col=False, names=col_names)
     return atac_table
 
+
 def add_GFP_wbid(atac_df: pd.DataFrame):
+    '''
+    Adds to the GFP transgene a wbid "GFP".
+
+    * inplace.
+    '''
     GFP_ind = atac_df[atac_df['symbol']=='GFP'].index
     atac_df.loc[GFP_ind,'wbid']='GFP'
 
 
 def drop_unneeded_columns(atac_df: pd.DataFrame):
     """
-    Drops 'feature_type' and 'symbol' columns, inplace
+    Drops 'feature_type' and 'symbol' columns.
+
+    * inplace
     """
     atac_df.drop(columns=["feature_type", "symbol"], inplace=True)
 
+
 def index_wbid(atac_df: pd.DataFrame):
     """
-    Sets the wbid column as the index, inplace
+    Sets the wbid column as the index.
+
+    * inplace
     """
     atac_df.set_index("wbid", inplace=True)
 
 
-def read_experiment(exp_name: str = "exp1"):
-    """
-    Gets an experiment name, reads all of the relevant tables to two lists, one for each condition.
 
-    exp_names can be: 'exp1' / 'exp_gonads' / 'exp_hrde_gonads' / 'exp_hrde_guy' / 'exp_metsetset'
+# def create_exp_dic(exp_dfs_cond1: list, exp_dfs_cond2: list, exp_name: str):
+#     """"""
+#     exp_dic = {}
+#     if exp_name == "exp1": # first GFP, than OMA-1
+#         exp_dic[0] = [exp_dfs_cond1[0], exp_dfs_cond2[0]]
+#         exp_dic[1] = [exp_dfs_cond1[1], exp_dfs_cond2[1]]
+#         exp_dic[2] = [exp_dfs_cond1[2], exp_dfs_cond2[2]]
+#         exp_dic[4] = [exp_dfs_cond1[3], exp_dfs_cond2[3]]
+#     else:  # if other experiments than the order is the same:
+#         for rep_num in range(3):
+#             exp_dic[rep_num] = [exp_dfs_cond1[rep_num], exp_dfs_cond2[rep_num]]
+#     return exp_dic
 
-    Return
-    -------
-    - exp_dfs_list: list of dfs, each is a sample of the desired experiment
-    """
-    #### file_names
-    exp1_files = [
-        ["DATA/ATAC_R0-iGFP.csv",
-        "DATA/ATAC_R1-iGFP.csv",
-        "DATA/ATAC_R2-iGFP.csv",
-        "DATA/ATAC_R4-iGFP.csv"],
-        ["DATA/ATAC_R0-iOMA-1.csv",
-        "DATA/ATAC_R1-iOMA-1.csv",
-        "DATA/ATAC_R2-iOMA-1.csv",
-        "DATA/ATAC_R4-iOMA-1.csv",]
-    ]
-    exp_gonads_files = []  # to fill later
-    exp_hrde_gonads_files = []  # to fill later
-    exp_hrde_guy_files = []  # to fill later
-    exp_metsetset_files = []  # to fill later
-
-    exps_dict = {
-        "exp1": exp1_files,
-        "exp_gonads": exp_gonads_files,
-        "exp_hrde_gonads": exp_hrde_gonads_files,
-        "exp_hrde_guy": exp_hrde_guy_files,
-        "exp_metsetset": exp_metsetset_files,
-    }
-
-    exp_dfs_cond1 = []
-    exp_dfs_cond2 = []
-    num_of_reps = len(exps_dict[exp_name][0])
-    for rep_i in range(num_of_reps):
-        exp_dfs_cond1.append(read_and_format_atac_table(exps_dict[exp_name][0][rep_i]))
-        exp_dfs_cond2.append(read_and_format_atac_table(exps_dict[exp_name][1][rep_i]))
-
-    return exp_dfs_cond1, exp_dfs_cond2
-
-
-def create_exp_dic(exp_dfs_cond1: list, exp_dfs_cond2: list, exp_name: str):
-    """"""
-    exp_dic = {}
-    if exp_name == "exp1": # first GFP, than OMA-1
-        exp_dic[0] = [exp_dfs_cond1[0], exp_dfs_cond2[0]]
-        exp_dic[1] = [exp_dfs_cond1[1], exp_dfs_cond2[1]]
-        exp_dic[2] = [exp_dfs_cond1[2], exp_dfs_cond2[2]]
-        exp_dic[4] = [exp_dfs_cond1[3], exp_dfs_cond2[3]]
-    else:  # if other experiments than the order is the same:
-        for rep_num in range(3):
-            exp_dic[rep_num] = [exp_dfs_cond1[rep_num], exp_dfs_cond2[rep_num]]
-    return exp_dic
-
-def create_exp_df(exp_dfs_cond1: list, exp_dfs_cond2: list, exp_name: str):
-    '''
-    '''
-    columns_dic = {'cond1':exp_dfs_cond1, 'cond2':exp_dfs_cond2}
-    exp_df = pd.DataFrame(columns_dic)
-    
-    if exp_name in ['exp1', 'exp_gonads', 'exp_metsetset']:
-        conds = {'cond1':'anti gfp','cond2':'anti OMA-1'}
-    elif exp_name in ['exp_hrde_gonads', 'exp_hrde_guy']:
-        conds = {'cond1':'SX','cond2':'hrde-1;SX'}
-    else:
-        NameError, 'exp name not recognized'
-    
-    exp_df.rename(columns=conds, inplace=True)
-    exp_df.index.name = 'rep'
-
-    return exp_df
-
-
-def read_experiment_to_dic(exp_name: str = "exp1"):
-    """"""
-    exp_dfs_cond1, exp_dfs_cond2 = read_experiment(exp_name)
-    exp_dic = create_exp_dic(exp_dfs_cond1, exp_dfs_cond2, exp_name)
-    return exp_dic
-
-def read_experiment_to_df(exp_name: str = "exp1"):
-    exp_dfs_cond1, exp_dfs_cond2 = read_experiment(exp_name)
-    exp_df = create_exp_df(exp_dfs_cond1, exp_dfs_cond2, exp_name)
-    return exp_df
+# def read_experiment_to_dic(exp_name: str = "exp1"):
+#     """"""
+#     exp_dfs_cond1, exp_dfs_cond2 = read_experiment(exp_name)
+#     exp_dic = create_exp_dic(exp_dfs_cond1, exp_dfs_cond2, exp_name)
+#     return exp_dic
 
 
 if __name__ == "__main__":
