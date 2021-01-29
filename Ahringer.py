002948bca792dbf539916e0ca08475b271511b17
Guy_Weintraub <guywine@gmail.com>
2021-01-29 12:08:16 +0200

    cleaned

--- Ahringer.py
+++ Ahringer.py
@@ -42,70 +42,25 @@ class Ahringer:
         return atac
 
     def _filter_atac_all(self):
+        '''
+        The Ahringer table consists of many peaks which are not assigned to any gene. 
+        This function filters two things:
+        a) Only peaks assigned to a gene
+        b) only peaks that are marked as "coding promoter"
+        '''
         atac_new = self.atac_all[
             self.atac_all["geneID"] != "."
         ]  # only peaks related to genes
         atac_new = atac_new[atac_new["regulatory_class"] == "coding_promoter"]
         return atac_new
 
-    def get_list(
-        self,
-        data_type: str,
-        col_name: str,
-        prcnt: float = 0,
-        bottom: bool = False,
-        thresh=None,
-        under_thresh: bool = False,
-    ):
-        """
-        Parameters
-        ----------
-        - col_name: str. name of columns
-        - data_type: str. ['rna' / 'atac']
-
-        Return
-        ----------
-        - gene_id_list: list.
-        """
-        if data_type.lower() == "rna":
-            data = self.rna
-        if data_type.lower() == "atac":
-            data = self.atac
-
-        col_orig = data[col_name].dropna()
-        if prcnt:
-            if not bottom:
-                # get upper percnage
-                quantile = col_orig.quantile(1 - (prcnt / 100))
-                print(f"quantile upper of {col_name} = {quantile}")
-                new_col = col_orig[col_orig > quantile]
-            else:
-                # get lower percentage
-                quantile = col_orig.quantile((prcnt / 100))
-                print(f"quantile lower of {col_name} = {quantile}")
-                new_col = col_orig[col_orig <= quantile]
-        else:
-            new_col = col_orig
-
-        if thresh is not None:
-            if under_thresh:
-                new_col = new_col[new_col < thresh]
-            else:
-                new_col = new_col[new_col > thresh]
-
-        index_list = list(new_col.index)
-
-        gene_id_list = list(data.loc[index_list, "geneID"])
-        gene_id_list = list(set(gene_id_list))
-
-        return gene_id_list
-
 
 def screen_genes_for_all_peaks(
     gene_list: list, ar: Ahringer, thresh: float, below_thresh: bool = False
 ):
     """
-    returns a list of gene names in which all atac peaks match threshold
+    Some genes in the table have multiple peaks assigned to them as promoters.
+    This function returns a list of gene names of which all of their atac peaks match the desired threshold.
     """
     gid = Gene_IDs()
     name_list = [gid.to_name(gene) for gene in gene_list if gid.to_name(gene)]
@@ -130,42 +85,13 @@ if __name__ == "__main__":
     import utilities as ut
 
     ar = Ahringer()
-    m = Table_mRNA()
-    gid = Gene_IDs()
-    ats = ATAC_signal()
 
-    ### new
-    germline_ahri_under5 = ut.get_list_of_column(
-        ar.rna["Germline"], thresh=5, under_thresh=True
-    )  # 9762
-    germline_ours_under5 = ut.get_list_of_column(
-        m.mean_exp, thresh=5, under_thresh=True
-    )  # 13954
-    germline_under5 = ut.intersect_lists(
-        germline_ahri_under5, germline_ours_under5
-    )  # 9539
+    # m = Table_mRNA()
 
-    atac_ahri_low = ut.get_list_of_column(
-        ar.atac["Germline"], prcnt=20, bottom=True
-    )  # 4478
+    # germline_ahri_under5 = ut.get_list_of_column(
+    #     ar.rna["Germline"], thresh=5, under_thresh=True
+    # )  # 9762
+    # germline_ours_under5 = ut.get_list_of_column(
+    #     m.mean_exp, thresh=5, under_thresh=True
+    # )  # 13954
 
-    igfp_mean_scores = ats.scores1.mean(axis=1)
-    ioma1_mean_scores = ats.scores2.mean(axis=1)
-    atac_ours_gfp_low = ut.get_list_of_column(
-        igfp_mean_scores, prcnt=20, bottom=True
-    )  #
-    atac_ours_oma1_low = ut.get_list_of_column(
-        ioma1_mean_scores, prcnt=20, bottom=True
-    )  #
-    atac_ours_low = ut.intersect_lists(atac_ours_gfp_low, atac_ours_oma1_low)  # 4008
-
-    atac_all_low = ut.intersect_lists(atac_ours_low, atac_ahri_low) # 411
-
-    low_rna_low_atac = ut.intersect_lists(germline_under5, atac_all_low) # 307
-
-    low_rna_low_atac_all_peaks = screen_genes_for_all_peaks(low_rna_low_atac, ar, 8.5, True) # 278
-
-
-    low_low_wbids = ut.list_to_wbids(low_rna_low_atac_all_peaks)
-
-    low_rna_low_atac_all_peaks_chrom_1 = [gene for gene in low_low_wbids if ar.rna.loc[gene,'chr']=='I']
