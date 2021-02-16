dab2b83c686d6638eb91062cd6e3609ec237f49d
Guy_Weintraub <guywine@gmail.com>
2021-02-16 12:40:57 +0200

    added "plot_range" option, pseudo-code for groups

--- plotting.py
+++ plotting.py
@@ -1,22 +1,30 @@
 import matplotlib.pyplot as plt
-import pandas as pd 
+import pandas as pd
 from gene_id import Gene_IDs
 import utilities as ut
 import seaborn as sns
 
-def plot_fc_gene(ATAC_exp1, ATAC_exp2, gene_name: str, std_flag: bool = True, log2_flag: bool = True):
-    '''
+
+def plot_fc_gene(
+    ATAC_exp1,
+    ATAC_exp2,
+    gene_name: str,
+    std_flag: bool = True,
+    log2_flag: bool = True,
+    plot_range: tuple = (0, 0),
+):
+    """
     Plots for a single gene panels, each line is the FC across the gene in one experiment.
-    - Single panel with FC (then mean) of all reps. 
+    - Single panel with FC (then mean) of all reps.
     - Panel for each replicate.
 
     Parameters
     --------
     - ATAC_exp1: ATAC_signal object. Contains ATAC-seq data of chosen experiment.
     - ATAC_exp2: "     "       "
-    - gene_name: str. 
+    - gene_name: str.
     - var_type: str. ['std'/'sem'/'none']. Which variance to plot. Only relevant if mean_flag True.
-    '''
+    """
     gene_fc_1 = ATAC_exp1.get_gean_fc_df(gene_name, log2=log2_flag)
     gene_fc_2 = ATAC_exp2.get_gean_fc_df(gene_name, log2=log2_flag)
 
@@ -24,23 +32,34 @@ def plot_fc_gene(ATAC_exp1, ATAC_exp2, gene_name: str, std_flag: bool = True, lo
     gene_fc_means[ATAC_exp1.exp_name] = gene_fc_1.mean(axis=1)
     gene_fc_means[ATAC_exp2.exp_name] = gene_fc_2.mean(axis=1)
 
-    if std_flag: # later - not working
+    if plot_range.count(0) != 2:  # if range was given:
+        gene_fc_means = narrow_to_range(gene_fc_means, plot_range[0], plot_range[1])
+
+    if std_flag:  # later - not working
         gene_fc_std = pd.DataFrame([])
         gene_fc_std[ATAC_exp1.exp_name] = gene_fc_1.std(axis=1)
         gene_fc_std[ATAC_exp2.exp_name] = gene_fc_2.std(axis=1)
+        if plot_range.count(0) != 2:  # if range was given:
+            gene_fc_std = narrow_to_range(gene_fc_std, plot_range[0], plot_range[1])
     else:
         gene_fc_std = 0
-    
+
     fig, ax0 = plt.subplots()
     plt.title(f"Fold-Change of signal for gene: {gene_name}", fontsize=14)
-    plt.xlabel('Location relative to TSS')
-    plt.ylabel('FC of ATAC-seq signal (norm.)')
+    plt.xlabel("Location relative to TSS")
+    plt.ylabel("FC of ATAC-seq signal (norm.)")
 
-    plot_ax(ax0, gene_fc_means, gene_fc_std) 
+    plot_ax(ax0, gene_fc_means, gene_fc_std)
 
 
-def plot_signal_gene(ATAC_exp, gene_name: str, mean_flag: bool = True, var_type: str = 'none', plot_range: tuple = (0,0)):
-    '''
+def plot_signal_gene(
+    ATAC_exp,
+    gene_name: str,
+    mean_flag: bool = True,
+    var_type: str = "none",
+    plot_range: tuple = (0, 0),
+):
+    """
     Plots for a single gene one of two options:
     - Single panel with mean and variance of this gene for all replicates (line per condition)
     - Panel for each replicate, with the gene's signal (line per condition)
@@ -48,22 +67,23 @@ def plot_signal_gene(ATAC_exp, gene_name: str, mean_flag: bool = True, var_type:
     Parameters
     --------
     - ATAC_exp: ATAC_signal object. Contains ATAC-seq data of chosen experiment.
-    - gene_name: str. 
+    - gene_name: str.
     - mean_flag: bool. If True, plots all replicates meaned on the same panel.
     - var_type: str. ['std'/'sem'/'none']. Which variance to plot. Only relevant if mean_flag True.
-    '''
+    """
 
     if mean_flag:
-        gene_means, gene_vars = ATAC_exp.get_gene_mean_and_var_both_conditions(gene_name, var_type)
+        gene_means, gene_vars = ATAC_exp.get_gene_mean_and_var_both_conditions(
+            gene_name, var_type
+        )
         fig, ax0 = plt.subplots()
         plt.title(f"Signal for gene: {gene_name}, {ATAC_exp.exp_name}", fontsize=14)
-        plt.xlabel('Location relative to TSS')
-        plt.ylabel('ATAC-seq signal (norm.)')
+        plt.xlabel("Location relative to TSS")
+        plt.ylabel("ATAC-seq signal (norm.)")
 
-        if plot_range.count(0)!=2: # if range was given:
+        if plot_range.count(0) != 2:  # if range was given:
             gene_means = narrow_to_range(gene_means, plot_range[0], plot_range[1])
             gene_vars = narrow_to_range(gene_vars, plot_range[0], plot_range[1])
-
 
         plot_ax(ax0, gene_means, gene_vars)
 
@@ -75,40 +95,96 @@ def plot_signal_gene(ATAC_exp, gene_name: str, mean_flag: bool = True, var_type:
 
         for rep_i in range(num_reps):
             gene_rep_df = pd.DataFrame([])
-            gene_rep_df[ATAC_exp.condition_names[0]] = ATAC_exp.cond1[rep_i].loc[wbid,:]
-            gene_rep_df[ATAC_exp.condition_names[1]] = ATAC_exp.cond2[rep_i].loc[wbid,:]
-            
-            if plot_range.count(0)!=2: # if range was given:
+            gene_rep_df[ATAC_exp.condition_names[0]] = ATAC_exp.cond1[rep_i].loc[
+                wbid, :
+            ]
+            gene_rep_df[ATAC_exp.condition_names[1]] = ATAC_exp.cond2[rep_i].loc[
+                wbid, :
+            ]
+
+            if plot_range.count(0) != 2:  # if range was given:
                 gene_rep_df = narrow_to_range(gene_rep_df, plot_range[0], plot_range[1])
-                
+
             list_of_rep_dfs.append(gene_rep_df)
 
-        fig, axes = plt.subplots(1,num_reps, figsize=(num_reps*6,5), sharey=True)
+        fig, axes = plt.subplots(1, num_reps, figsize=(num_reps * 6, 5), sharey=True)
         for rep_i in range(num_reps):
-            axes[rep_i].set_title(f'replicate {rep_i+1}')
+            axes[rep_i].set_title(f"replicate {rep_i+1}")
             ## later - add y title and x title
             legend_flag = False
-            if rep_i==num_reps-1:
-                legend_flag=True
+            if rep_i == num_reps - 1:
+                legend_flag = True
             plot_ax(axes[rep_i], list_of_rep_dfs[rep_i], legend_flag)
 
 
-def plot_groups_signals():
-    '''
-    '''
+def plot_groups_signals(
+    ATAC_exp,
+    groups_dic: dict,
+    mean_flag: bool = False,
+    var_type: str = "std",
+    add_highly_lowly: bool = True,
+):
+    """
+    Takes an experiment, a dictionary with groups, plots panel with two axes.
+    - one ax for each condition: each group is a line with std.
+
+    If mean_flag: variance is between reps. if seperate: variance is between genes.
+    """
+    # if not mean_flag:
+        # create figure (1,2)
+        # for each condition (sample) (get for this sample a df of means, and df of vars):
+            # initiate means_df
+            # initiate vars_df
+            # for each group: *** add highly, lowly ****
+                # "group_mean_and_var_for_sample"
+                # add group_mean to means_df['group_name']
+                # add group_var to vars_df['group_name']
+
+            # plot
+
+    # If mean_flag:
+        # create figure (1,2)
+        # for each condition:
+            # initiate means_df_list
+
+            # for each rep (sample):
+                # initiate means_df_rep
+                # for each group:
+                    # "group_mean_and_var_for_sample" [only means]
+                    # add group to means_df_rep['group_name']
+                # add means_df_rep to means_df_list
+
+        # use "get_mean_variance" to get mean and var of groups from all reps
+
+        # plot ax of condition
+
     pass
 
+
+def group_mean_and_var_for_sample(df_sample, gene_list, var_type="std"):
+    """
+    Gets a list of genes, and a df sample, returns the means and vars.
+
+    return
+    --------
+    - group_mean: pd.Series, mean along gene
+    - group_var: pd.Series, var along gene. If var_type='none', returns 0.
+    """
+    pass
+
+
 def narrow_to_range(df, first_row, last_row):
-    '''
+    """
     Narrows df of signal by locations (relative to tss).
     Inclusive.
-    '''
-    df.index = df.index.astype('int64')
+    """
+    df.index = df.index.astype("int64")
 
     return df.loc[first_row:last_row, :]
 
-def plot_ax(ax, vec_df, var_df=0, legend_flag:bool = True):
-    '''
+
+def plot_ax(ax, vec_df, var_df=0, legend_flag: bool = True):
+    """
     Plots all lines in vec_df columns (with legend)
     Adds fill_between of var_df if not 0.
     Adds legend if specified.
@@ -121,23 +197,28 @@ def plot_ax(ax, vec_df, var_df=0, legend_flag:bool = True):
     - legend_flag: bool. If true, plot legend on this axis.
 
     * No return - plots on ax.
-    '''
+    """
     lines = []
-    colors = plt.get_cmap('Accent') # later (define outside?)
+    colors = plt.get_cmap("Accent")  # later (define outside?)
 
-    if isinstance(var_df, int): # if no varince df given
+    if isinstance(var_df, int):  # if no varince df given
         for col_i in range(vec_df.shape[1]):
-            line = plot_vector(vec=vec_df.iloc[:,col_i], ax=ax, color=colors(col_i))
+            line = plot_vector(vec=vec_df.iloc[:, col_i], ax=ax, color=colors(col_i))
             lines.append(line)
-    else:   # if svariance given
+    else:  # if svariance given
         for col_i in range(vec_df.shape[1]):
-            line = plot_vector(vec=vec_df.iloc[:,col_i], ax=ax, color=colors(col_i), var_vec=var_df.iloc[:,col_i])
+            line = plot_vector(
+                vec=vec_df.iloc[:, col_i],
+                ax=ax,
+                color=colors(col_i),
+                var_vec=var_df.iloc[:, col_i],
+            )
             lines.append(line)
-    
+
     if legend_flag:
         first_leg = plt.legend(lines, vec_df.columns)
         ax = plt.gca().add_artist(first_leg)
-    
+
 
 def plot_vector(vec, ax, color, var_vec=0):
     """
@@ -148,11 +229,10 @@ def plot_vector(vec, ax, color, var_vec=0):
 
     line = ax.plot(vec, c=color)
     if not isinstance(var_vec, int):
-        ax.fill_between(vec.index.astype('int64'), vec-var_vec, vec+var_vec, fc=color, alpha=0.3)
+        ax.fill_between(
+            vec.index.astype("int64"), vec - var_vec, vec + var_vec, fc=color, alpha=0.3
+        )
     return line[0]
-
-
-
 
 
 ####################################################################
@@ -161,7 +241,10 @@ def plot_vector(vec, ax, color, var_vec=0):
 # func1: from series, name, and gene list - return ax with histogram and markings.
 # func2: from df with multiple series, use func 1 to create multiple panels, and a legend.
 
-def plot_gene_atac_signal_histogram(ATAC_exp, gene_to_mark: str, mean_flag: bool = True):
+
+def plot_gene_atac_signal_histogram(
+    ATAC_exp, gene_to_mark: str, mean_flag: bool = True
+):
     if mean_flag:
         mean_df = pd.DataFrame({"mean_FC": ATAC_exp.fc.mean(axis=1)})
         plot_reps_hist_mark_gene(df_reps=mean_df, genes_to_mark=gene_to_mark)
